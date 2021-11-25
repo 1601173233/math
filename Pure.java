ac4687b68a9b07afa0886f536a8e52f97a4f5def
1601173233 <1601173233@qq.com>
2021-11-25 17:29:09 +0800

    Add files via upload

---
+++ Pure.java
@@ -0,0 +1,170 @@
+package planningmanagement;
+
+/**
+ * 单纯形式
+ */
+public class Pure {
+
+    /**
+     * 计算线性规划
+     *
+     * @param conditionArray 条件参数
+     * @param conditionResult 条件结果矩阵
+     * @param resultArray 结果参数
+     * @return 结果集
+     */
+    public static Integer getMax(double[][] conditionArray, double[] conditionResult, double[] resultArray) {
+        double[] conditionParams = new double[conditionArray[0].length];
+        int[] notNullIndex = new int[conditionResult.length];
+        int[] nullIndex = new int[conditionResult.length];
+        int best = 0;
+
+        for (int i = 0; i < nullIndex.length; i++) {
+            nullIndex[i] = i;
+        }
+
+        for (int i = 0; i < notNullIndex.length; i++) {
+            notNullIndex[i] = conditionParams.length + i - notNullIndex.length;
+        }
+
+        // 初始解
+        for (int i = 0; i < notNullIndex.length; i++) {
+            conditionParams[notNullIndex[i]] = conditionResult[i];
+        }
+
+        // 出入基
+        while (!isBest(resultArray)) {
+            // 入基下标
+            int inIndex = getInIndex(nullIndex, resultArray);
+
+            // 出基下标
+            int outIndex = getOutIndex(conditionArray, conditionResult, notNullIndex, nullIndex, inIndex);
+
+            int nullIndexNum = nullIndex[inIndex];
+            int notNullIndexNum = notNullIndex[outIndex];
+
+            notNullIndex[outIndex] = nullIndexNum;
+            nullIndex[outIndex] = notNullIndexNum;
+
+            double conditionNum = conditionArray[outIndex][inIndex];
+            for (int i = 0; i < conditionArray[0].length; i++) {
+                conditionArray[outIndex][i] /= conditionNum;
+            }
+
+            conditionResult[outIndex] = conditionResult[outIndex] / conditionNum;
+
+            for (int i = 0; i < conditionArray.length; i++) {
+                if (i != outIndex) {
+                    double outConditionNum = conditionArray[i][outIndex];
+                    for (int j = 0; j < conditionArray[0].length; j++) {
+                        conditionArray[i][j] += - outConditionNum * conditionArray[outIndex][j];
+                    }
+
+                    conditionResult[i] += -outConditionNum * conditionResult[outIndex];
+                }
+            }
+
+            double outConditionNum = resultArray[outIndex];
+            for (int i = 0; i < resultArray.length; i++) {
+                resultArray[i] += -outConditionNum * conditionArray[outIndex][i];
+            }
+
+            best += -outConditionNum * conditionResult[outIndex];
+        }
+
+        return best;
+    }
+
+    /**
+     * 测试是否为最优解
+     *
+     */
+    public static boolean isBest(double[] resultArray) {
+        for (double i : resultArray) {
+            if (i < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 获取入基变量
+     *
+     * @param nullIndex 非基变量数组
+     * @param resultArray 结果系数
+     * @return 入基变量
+     */
+    public static int getInIndex(int[] nullIndex, double[] resultArray) {
+        // 获取入基变量
+        int inIndex = 0;
+        double inMin = 0;
+        for (int i = 0; i < nullIndex.length; i++) {
+            int index = nullIndex[i];
+            if (resultArray[index] < 0 && resultArray[index] < inMin) {
+                inIndex = i;
+                inMin = resultArray[index];
+            }
+        }
+
+        return inIndex;
+    }
+
+    /**
+     * 获取出基变量
+     *
+     * @param conditionArray 条件系数
+     * @param conditionResult 条件结果
+     * @param notNullIndex 基变量数组
+     * @param nullIndex 非基变量数组
+     * @param inIndex 非基变量数组下标
+     * @return 出基变量
+     */
+    public static int getOutIndex(double[][] conditionArray,
+                                  double[] conditionResult,
+                                  int[] notNullIndex,
+                                  int[] nullIndex,
+                                  int inIndex) {
+
+        int outIndex = 0;
+        double outMin = Double.MAX_VALUE;
+        for (int i = 0; i < notNullIndex.length; i++) {
+            if (conditionArray[i][inIndex] == 0) {
+                continue;
+            }
+
+            double temp = conditionResult[i] / conditionArray[i][nullIndex[inIndex]];
+
+            if (temp > 0 && temp < outMin) {
+                outIndex = i;
+                outMin = temp;
+            }
+        }
+
+        return outIndex;
+    }
+
+    public static void main(String[] args) {
+        double[][] conditionArray = new double[][]{{6, 4}, {1, 2}, {-1, 1}, {0, 1}};
+        double[] conditionResult = new double[]{24, 6, 1, 2};
+        double[] resultArray = new double[]{-5, -4};
+
+        double[][] mConditionArray = new double[conditionArray.length][conditionArray.length + conditionArray[0].length];
+        double[] mResultArray = new double[conditionArray.length + resultArray.length];
+
+        for (int i = 0; i < conditionArray.length; i++) {
+            for (int j = 0; j < conditionArray[0].length; j++) {
+                mConditionArray[i][j] = conditionArray[i][j];
+            }
+
+            mConditionArray[i][conditionArray[0].length + i] = 1;
+        }
+
+        for (int i = 0; i < resultArray.length; i++) {
+            mResultArray[i] = resultArray[i];
+        }
+
+        System.out.println(getMax(mConditionArray, conditionResult, mResultArray));
+    }
+}
---
+++ TransportationTable.java
@@ -0,0 +1,391 @@
+package planningmanagement;
+
+import com.sun.org.apache.xpath.internal.operations.Or;
+
+/**
+ * 单纯形式
+ */
+public class TransportationTable {
+
+    /**
+     * 计算线性规划
+     *
+     * @param conditionResult 条件结果矩阵
+     * @param xResultArray 横结果参数
+     * @param yResultArray 列结果参数
+     * @return 结果集
+     */
+    public static Double getMax(double[][] conditionResult, double[] xResultArray, double[] yResultArray) {
+
+        // 1.获取初始解
+        boolean[][] baseArray = new boolean[xResultArray.length][yResultArray.length];
+        double[][] baseResultArray = getFirstResult(xResultArray, yResultArray, baseArray);
+
+        System.out.println();
+        printResult(baseResultArray);
+        System.out.println();
+
+        while (true) {
+            // 2.计算乘子
+            double[] xMultiplicator = new double[xResultArray.length];
+            double[] yMultiplicator = new double[yResultArray.length];
+            getMultiplicator(conditionResult, baseArray, xMultiplicator, yMultiplicator);
+
+            // 3.计算非基变量计算 ui + vj - cij
+            double[][] notBaseVarArray = calNotBaseVar(conditionResult, baseArray, xMultiplicator, yMultiplicator);
+            int maxX = 0;
+            int maxY = 0;
+            double maxNotBaseVar = 0;
+            for (int xIndex = 0; xIndex < notBaseVarArray.length; xIndex++) {
+                for (int yIndex = 0; yIndex < notBaseVarArray[0].length; yIndex++) {
+                    if (maxNotBaseVar < notBaseVarArray[xIndex][yIndex]) {
+                        maxNotBaseVar = notBaseVarArray[xIndex][yIndex];
+                        maxX = xIndex;
+                        maxY = yIndex;
+                    }
+                }
+            }
+
+            if (maxNotBaseVar == 0) {
+                break;
+            }
+
+            baseArray[maxX][maxY] = true;
+            int[][] closedLoop = searchClosedLoop(baseArray, maxX, maxY);
+
+            int minX = 0;
+            int minY = 0;
+            double min = Double.MAX_VALUE;
+            for (int x = 0; x < closedLoop.length; x++) {
+                for (int y = 0; y < closedLoop[x].length; y++) {
+                    if (closedLoop[x][y] == 1 && min > baseResultArray[x][y]) {
+                        minX = x;
+                        minY = y;
+                        min = baseResultArray[x][y];
+                    }
+                }
+            }
+
+            for (int x = 0; x < closedLoop.length; x++) {
+                for (int y = 0; y < closedLoop[x].length; y++) {
+                    if (closedLoop[x][y] == 1) {
+                        baseResultArray[x][y] -= min;
+                    } else if(closedLoop[x][y] == 2) {
+                        baseResultArray[x][y] += min;
+                    }
+                }
+            }
+
+            baseArray[minX][minY] = false;
+            baseResultArray[maxX][maxY] = min;
+
+            System.out.println();
+            printResult(baseResultArray);
+            System.out.println();
+        }
+
+        double result = 0;
+        for (int xIndex = 0; xIndex < baseResultArray.length; xIndex++) {
+            for (int yIndex = 0; yIndex < baseResultArray[0].length; yIndex++) {
+                result += conditionResult[xIndex][yIndex] * baseResultArray[xIndex][yIndex];
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 获取初始解
+     *
+     * 左上角法
+     *
+     * @param baseArray 进基变量矩阵
+     * @return 初始解矩阵
+     */
+    public static double[][] getFirstResult(double[] xResultArray,
+                                            double[] yResultArray,
+                                            boolean[][] baseArray) {
+        double[][] result = new double[xResultArray.length][yResultArray.length];
+
+        double[] xArray = copy(xResultArray), yArray = copy(yResultArray);
+
+        int xIndex = 0, yIndex = 0;
+        while (xIndex < xArray.length && yIndex < yArray.length) {
+            result[xIndex][yIndex] = Math.min(xArray[xIndex], yArray[yIndex]);
+            baseArray[xIndex][yIndex] = true;
+
+            if (xArray[xIndex] == result[xIndex][yIndex]) {
+                yArray[yIndex] -= result[xIndex][yIndex];
+                xIndex++;
+            } else {
+                xArray[xIndex] -= result[xIndex][yIndex];
+                yIndex++;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 计算乘子
+     *
+     * @param conditionResult 条件结果矩阵
+     * @param baseArray 进基变量矩阵
+     * @param xMultiplicator 横乘子结果参数
+     * @param yMultiplicator 列乘子结果参数
+     */
+    private static void getMultiplicator(double[][] conditionResult,
+                                         boolean[][] baseArray,
+                                         double[] xMultiplicator,
+                                         double[] yMultiplicator) {
+        int[] xHasResultArray = new int[xMultiplicator.length], yHasResultArray = new int[yMultiplicator.length];
+
+        xMultiplicator[0] = 0;
+        xHasResultArray[0] = 1;
+
+        boolean isX = true;
+        int count = 0;
+        int maxCount = xMultiplicator.length * yMultiplicator.length;
+        while (!getMultiplicatorFinish(xHasResultArray, yHasResultArray) && count++ < maxCount) {
+            if (isX) {
+                for (int xIndex = 0; xIndex < xHasResultArray.length; xIndex++) {
+                    if (xHasResultArray[xIndex] == 1) {
+                        for (int yIndex = 0; yIndex < yHasResultArray.length; yIndex++) {
+                            if (yHasResultArray[yIndex] == 0 && baseArray[xIndex][yIndex]) {
+                                yMultiplicator[yIndex] = conditionResult[xIndex][yIndex] - xMultiplicator[xIndex];
+                                yHasResultArray[yIndex] = 1;
+                            }
+                        }
+
+                        xHasResultArray[xIndex] = 2;
+                    }
+                }
+            } else {
+                for (int yIndex = 0; yIndex < yHasResultArray.length; yIndex++) {
+                    if (yHasResultArray[yIndex] == 1) {
+                        for (int xIndex = 0; xIndex < xHasResultArray.length; xIndex++) {
+                            if (xHasResultArray[xIndex] == 0 && baseArray[xIndex][yIndex]) {
+                                xMultiplicator[xIndex] = conditionResult[xIndex][yIndex] - yMultiplicator[yIndex];
+                                xHasResultArray[xIndex] = 1;
+                            }
+                        }
+
+                        yHasResultArray[yIndex] = 2;
+                    }
+                }
+            }
+
+            isX = !isX;
+        }
+    }
+
+    /**
+     * 校验是否已经完成了
+     *
+     * @param xHasResultArray x结果
+     * @param yHasResultArray y结果
+     * @return 已完成
+     */
+    private static boolean getMultiplicatorFinish(int[] xHasResultArray, int[] yHasResultArray) {
+        for (int result : xHasResultArray) {
+            if (result == 0) {
+                return false;
+            }
+        }
+
+        for (int result : yHasResultArray) {
+            if (result == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 计算非基变量计算 ui + vj - cij
+     *
+     * @param conditionResult 条件结果矩阵
+     * @param baseArray 基变量
+     * @param xMultiplicator 横乘子结果参数
+     * @param yMultiplicator 列乘子结果参数
+     * @return 已完成
+     */
+    private static double[][] calNotBaseVar(double[][] conditionResult,
+                                            boolean[][] baseArray,
+                                            double[] xMultiplicator,
+                                            double[] yMultiplicator) {
+        double[][] result = new double[xMultiplicator.length][yMultiplicator.length];
+
+        for (int xIndex = 0; xIndex < xMultiplicator.length; xIndex++) {
+            for (int yIndex = 0; yIndex < yMultiplicator.length; yIndex++) {
+                if (!baseArray[xIndex][yIndex]) {
+                    result[xIndex][yIndex] = xMultiplicator[xIndex] + yMultiplicator[yIndex] - conditionResult[xIndex][yIndex];
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 寻找闭环
+     *
+     * @param baseArray 进基数组
+     * @param xIndex 初始下标x
+     * @param yIndex 初始下标y
+     * @return
+     */
+    private static int[][] searchClosedLoop(boolean[][] baseArray, int xIndex, int yIndex) {
+        int[][] result = new int[baseArray.length][baseArray[0].length];
+
+        result[xIndex][yIndex] = -1; // 开始节点
+
+        searchClosedLoop(baseArray, result, -1, xIndex, yIndex, true);
+        return result;
+    }
+
+    /**
+     * 寻找闭环
+     *
+     * @param baseArray 进基数组
+     * @param xIndex 初始下标x
+     * @param yIndex 初始下标y
+     * @return
+     */
+    private static boolean searchClosedLoop(boolean[][] baseArray,
+                                            int[][] result,
+                                            int orientation,
+                                            int xIndex,
+                                            int yIndex,
+                                            boolean type) {
+        for (int i = 0; i < 4; i++) {
+            if (i == orientation || i == orientation - 2 || i == orientation + 2) {
+                continue;
+            }
+
+            switch (i) {
+                case 0:
+                for (int x = xIndex + 1; x < baseArray.length; x++) {
+                    if (searchCloseLoopCheck(baseArray, result, i, x, yIndex, type)) {
+                        return true;
+                    }
+                }
+                break;
+                case 1:
+                    for (int y = yIndex + 1; y < baseArray[0].length; y++) {
+                        if (searchCloseLoopCheck(baseArray, result, i, xIndex, y, type)) {
+                            return true;
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int x = xIndex - 1; x >= 0; x--) {
+                        if (searchCloseLoopCheck(baseArray, result, i, x, yIndex, type)) {
+                            return true;
+                        }
+                    }
+                    break;
+                case 3:
+                    for (int y = yIndex - 1; y >= 0; y--) {
+                        if (searchCloseLoopCheck(baseArray, result, i, xIndex, y, type)) {
+                            return true;
+                        }
+                    }
+                    break;
+                default:
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 闭环校验
+     *
+     * @param baseArray 数组
+     * @param result 结果集
+     * @param i 方向
+     * @param x x坐标
+     * @param y y坐标
+     * @return 闭环校验
+     */
+    private static Boolean searchCloseLoopCheck(boolean[][] baseArray,
+                                                int[][] result,
+                                                int i,
+                                                int x,
+                                                int y,
+                                                boolean type) {
+        if (baseArray[x][y]) {
+            if (result[x][y] == -1) {
+                return true;
+            } else if (result[x][y] >= 1) {
+                return false;
+            }
+
+            result[x][y] = type ? 1 : 2;
+            if (searchClosedLoop(baseArray, result, i, x, y, !type)) {
+                return true;
+            } else {
+                result[x][y] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * 数组复制
+     *
+     * @param array 原数组
+     * @return 目标数组
+     */
+    private static double[] copy(double[] array) {
+        double[] result = new double[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i];
+        }
+
+        return array;
+    }
+
+    /**
+     * 打印结果
+     *
+     * @param result 结果
+     */
+    private static void printResult(double[] result) {
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * 打印结果
+     *
+     * @param result 结果
+     */
+    private static void printResult(double[][] result) {
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+//        double[][] conditionResult = {{10, 2, 20, 11}, {12, 7, 9, 20}, {4, 14, 16, 18}};
+//        double[] xResultArray = {15, 25, 10};
+//        double[] yResultArray = {5, 15, 15, 15d};
+
+        double[][] conditionResult = {{400, 800}, {300, 500}};
+        double[] xResultArray = {12, 6};
+        double[] yResultArray = {10, 8};
+
+        System.out.println(400 * 6 + 800 * 6 + 300 * 4 + 200 * 500);
+        System.out.println(getMax(conditionResult, xResultArray, yResultArray));
+    }
+}
